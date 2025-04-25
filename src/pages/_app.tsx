6c07aacba947b6d61ceb94c0efb869da385a59cb
apunkto <apunkto@gmail.com>
2025-04-25 10:58:37 +0300

    ctp page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider, CssBaseline } from '@mui/material'
 import type { AppProps } from 'next/app'
 import theme from '@/lib/theme'
+import '@/styles/fonts.css'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
-                      rel="stylesheet"/>
+
             </Head>
             <body>
             <Main/>
