79cca90d9698d73dccc98dd9d39fb5ff3b28d19a
apunkto <apunkto@gmail.com>
2025-04-25 10:22:36 +0300

    ctp page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,18 @@
-import { AppBar, Toolbar, Typography, Container, Box } from '@mui/material'
-import { ReactNode } from 'react'
+import {AppBar, Toolbar, Typography, Container, Box} from '@mui/material'
+import {ReactNode} from 'react'
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({children}: { children: ReactNode }) {
     return (
         <>
             <AppBar position="static">
                 <Toolbar>
-                    <Box component="img" src="/jj100_logo.svg" alt="Logo" sx={{ height: 40, mr: 2 }} />
-                    <Typography variant="h6">Järva-Jaani 100!</Typography>
+                    <Box sx={{width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                        <Box component="img" src="/logo.png" alt="Logo" sx={{height: 40, mr: 2}}/>
+                        <Typography variant="h6">Järva-Jaani 100!</Typography>
+                    </Box>
                 </Toolbar>
             </AppBar>
-            <Container sx={{ mt: 4 }}>
+            <Container sx={{mt: 4}}>
                 {children}
             </Container>
         </>
