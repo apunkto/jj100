62409f2b7199679af2ef668ad92821d2b792fead
apunkto <apunkto@gmail.com>
2025-04-30 11:06:47 +0300

    add to pages

--- src/pages/course/index.tsx
+++ src/pages/course/index.tsx
@@ -1,21 +1,21 @@
-import {useCallback, useEffect, useRef, useState} from 'react'
-import {Swiper, SwiperSlide} from 'swiper/react'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/navigation'
-import {Navigation} from 'swiper/modules'
+import { Navigation } from 'swiper/modules'
 import Layout from '@/src/components/Layout'
 import Image from 'next/image'
-import {Box, IconButton, Typography, TextField, Button} from '@mui/material'
+import { Box, IconButton, Typography, TextField, Button } from '@mui/material'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
-import useCtpApi, {HoleResult} from '@/src/api/useCtpApi'
-import {debounce} from 'lodash'
+import useCtpApi, { HoleResult } from '@/src/api/useCtpApi'
+import { debounce } from 'lodash'
 
 export default function CoursePage() {
     const totalCards = 100
-    const cards = Array.from({length: totalCards}, (_, i) => i + 1)
+    const cards = Array.from({ length: totalCards }, (_, i) => i + 1)
 
-    const {getHole} = useCtpApi()
+    const { getHole } = useCtpApi()
 
     const prevRef = useRef<HTMLButtonElement | null>(null)
     const nextRef = useRef<HTMLButtonElement | null>(null)
@@ -26,6 +26,7 @@ export default function CoursePage() {
 
     const [searchInput, setSearchInput] = useState<string>('')
 
+    const excludedHoleNumbers = [76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 87, 88, 89, 90, 91, 94]
 
     useEffect(() => {
         if (swiperInstance && prevRef.current && nextRef.current) {
@@ -51,20 +52,16 @@ export default function CoursePage() {
         if (currentHoleNumber >= 1 && currentHoleNumber <= totalCards) {
             loadHole(currentHoleNumber)
 
-            // Preload previous hole if it exists
             if (currentHoleNumber > 1) {
                 loadHole(currentHoleNumber - 1)
             }
 
-            // Preload next hole if it exists
             if (currentHoleNumber < totalCards) {
                 loadHole(currentHoleNumber + 1)
             }
         }
     }, [currentHoleNumber, getHole, holeInfo])
 
-
-    // Debounced function
     const debouncedSlideTo = useCallback(
         debounce((holeNumber: number) => {
             if (swiperInstance && holeNumber >= 1 && holeNumber <= totalCards) {
@@ -77,7 +74,6 @@ export default function CoursePage() {
     useEffect(() => {
         return () => debouncedSlideTo.cancel()
     }, [debouncedSlideTo])
-
 
     const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value
@@ -92,7 +88,6 @@ export default function CoursePage() {
     return (
         <Layout>
             <Box mt={0}>
-                {/* Header with Title and Search */}
                 <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
                     <Typography variant="h4" fontWeight="bold">
                         Rada
@@ -115,115 +110,110 @@ export default function CoursePage() {
                         }}
                         inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
                     />
-
                 </Box>
 
                 <Swiper
                     modules={[Navigation]}
-                    spaceBetween={10}  // Space between slides
-                    centeredSlides={true}  // Center active slide
-                    slidesPerView={"auto"}  // Each slide can have its own width
+                    spaceBetween={10}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
                     onSwiper={(swiper) => setSwiperInstance(swiper)}
                     onSlideChange={(swiper) => setCurrentHoleNumber(swiper.activeIndex + 1)}
-                    style={{maxWidth: '550px', margin: '0 auto', width: '100%'}}
+                    style={{ maxWidth: '550px', margin: '0 auto', width: '100%' }}
                 >
                     {cards.map((number) => (
-                        <SwiperSlide key={number} style={{width: '90%'}}>
-
+                        <SwiperSlide key={number} style={{ width: '90%' }}>
                             <Box
                                 sx={{
                                     position: 'relative',
                                     width: '100%',
-                                    paddingTop: '141.4%', // maintain aspect ratio
+                                    paddingTop: '141.4%',
                                     borderRadius: '15px',
                                     overflow: 'hidden',
                                 }}
                             >
-                                <Box
-                                    sx={{
-                                        position: 'absolute',
-                                        zIndex: 2,
-                                        right: '12%',
-                                        top: '7.5%',
-                                        transform: 'translateY(-50%)',  // Pull up by 50% of its own height
-                                    }}
-                                >
-                                    <Typography
-                                        variant="h4"
-                                        fontWeight="bold"
+                                {!excludedHoleNumbers.includes(number) && holeInfo[number]?.hole.length && (
+                                    <Box
                                         sx={{
-                                            fontSize: 'clamp(4px, 4vw, 24px)',
-                                            color: 'black',
-                                            fontFamily: 'Alatsi, sans-serif',
+                                            position: 'absolute',
+                                            zIndex: 2,
+                                            right: '12.5%',
+                                            top: '7.5%',
+                                            transform: 'translateY(-50%)',
                                         }}
                                     >
-                                        {holeInfo[number]?.hole.length}
-                                    </Typography>
-                                </Box>
+                                        <Typography
+                                            variant="h4"
+                                            fontWeight="regular"
+                                            sx={{
+                                                fontSize: 'clamp(4px, 4vw, 24px)',
+                                                color: 'black',
+                                                fontFamily: 'Alatsi, sans-serif',
+                                            }}
+                                        >
+                                            {holeInfo[number]?.hole.length}
+                                        </Typography>
+                                    </Box>
+                                )}
                                 <Image
                                     src={`/cards/${number}.webp`}
                                     alt={`Rada ${number}`}
                                     layout="fill"
                                     objectFit="cover"
                                     style={{
-                                        borderRadius: '10px', // <-- directly on the img
+                                        borderRadius: '10px',
                                     }}
                                     sizes="(max-width: 600px) 100vw, 500px"
                                 />
                             </Box>
                         </SwiperSlide>
-
-
                     ))}
                 </Swiper>
 
                 <Box display="flex" justifyContent="space-between" alignItems="center" gap={2} mt={1}>
                     <IconButton color="primary" ref={prevRef}>
-                        <ArrowBackIosNewIcon/>
+                        <ArrowBackIosNewIcon />
                     </IconButton>
                     {holeInfo[currentHoleNumber]?.hole.coordinates && (
                         <Box>
-
-                            <Button variant="outlined" color="primary" size="small"
-                                    onClick={() => {
-                                        const coords = holeInfo[currentHoleNumber]!.hole.coordinates
-                                        const url = `https://www.google.com/maps/dir/?api=1&destination=${coords}&travelmode=walking`
-
-                                        window.open(
-                                            url,
-                                            '_blank'
-                                        )
-                                    }}
+                            <Button
+                                variant="outlined"
+                                color="primary"
+                                size="small"
+                                onClick={() => {
+                                    const coords = holeInfo[currentHoleNumber]!.hole.coordinates
+                                    const url = `https://www.google.com/maps/dir/?api=1&destination=${coords}&travelmode=walking`
+                                    window.open(url, '_blank')
+                                }}
                             >
                                 📍 Juhata rajale
                             </Button>
                         </Box>
                     )}
                     <IconButton color="primary" ref={nextRef}>
-                        <ArrowForwardIosIcon/>
+                        <ArrowForwardIosIcon />
                     </IconButton>
                 </Box>
 
                 {holeInfo[currentHoleNumber] && (
                     <Box mt={1} textAlign="center">
-                        {holeInfo[currentHoleNumber]?.hole.is_ctp ? (
+                        {holeInfo[currentHoleNumber]?.hole.is_ctp && (
                             <>
-                                <Typography color="primary">
-                                    🎯 Sellel korvil on CTP
-                                </Typography>
+                                <Typography color="primary">🎯 Sellel korvil on CTP</Typography>
                                 <Box mt={1}>
-                                    <Button variant="contained" color="primary" size="small"
-                                            onClick={() => {
-                                                window.location.href = `/ctp/${currentHoleNumber}`
-                                            }}
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        size="small"
+                                        onClick={() => {
+                                            window.location.href = `/ctp/${currentHoleNumber}`
+                                        }}
                                     >
                                         Märgi CTP
                                     </Button>
                                 </Box>
                             </>
-                        ) : ''}
-
-
+                        )}
                     </Box>
                 )}
             </Box>
