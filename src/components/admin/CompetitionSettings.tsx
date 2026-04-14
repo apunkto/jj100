14627ea8cbe8e48c6e6e1dd602edc715f0349be3
apunkto <apunkto@gmail.com>
2026-04-14 12:52:18 +0300

    fix initial slider issue

--- src/components/admin/CompetitionSettings.tsx
+++ src/components/admin/CompetitionSettings.tsx
@@ -93,7 +93,7 @@ export function CompetitionSettings({
                             disabled={updating}
                         />
                     }
-                    label="Toitlustuse valikud"
+                    label="Luba toidu valik"
                 />
 
                 <FormControlLabel
